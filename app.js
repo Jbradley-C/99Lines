7d8f44780dedbb479b0dfc123768f8219eda73d2
Jbradley-C <jbradley.chapman@gmail.com>
2020-05-22 13:57:22 -0500

    fixed generated numbers in the out put text and formatted text

--- app.js
+++ app.js
@@ -2,45 +2,22 @@ let Friends = [ 'John', 'Jacob', 'Jingle', 'Himer', 'Schmitt' ];
 
 //Loops for the songs
 for (let i = 0; i < Friends.length; i++) {
-	console.log(Friends[i] + ':');
-	let num = 99;
-	while (num >= 1) {
-		if (num > 2)
+	console.log(Friends[i + ':']);
+
+	for (let num = 99; num > 0; num--) {
+		if (num === 2) {
 			console.log(
-				num +
-					' Lines of code in the file! ' +
-					num +
-					' Lines of Code! ' +
-					Friends[i] +
-					' deletes one line, clears it all out.' +
-					'.. ' +
-					(num - 1) +
-					' Lines of code in the file!'
-			);
-		else if (num === 2)
-			console.log(
-				num +
-					' Lines of code in the file! ' +
-					num +
-					' Lines of Code! ' +
-					Friends[i] +
-					' deletes one line, clears it all out.' +
-					'.. ' +
-					(num - 1) +
-					' Line of code in the file!'
-			);
-		else if (num === 1)
-			console.log(
-				num +
-					'  Line of code in the file! ' +
-					num +
-					' Line of Code! ' +
-					Friends[i] +
-					' deletes one line, clears it all out.' +
-					'.. ' +
-					' No more lines of code in the file!'
+				num + ' Lines of code in the file! ' + num + ' Lines of Code! ' + Friends[i] + ' deletes one line, clears it all out.' + '.. ' +	(num - 1) +  ' Line of code in the file!'
 			);
 
-		num = num - 1;
+		} else if (num === 1) {
+			console.log(
+				num + ' Lines of code in the file! ' +	num + ' Lines of Code! ' + Friends[i] + ' deletes one line, clears it all out.' + '.. ' + ' no more lines of code in the file!' 
+			);
+
+		} else {
+			console.log(num + '  Lines of code in the file! ' + num + ' Line of Code! ' + Friends[i] + ' deletes one line, clears it all out.' + '.. ' +  (num - 1) + '  lines of code in the file!'
+			);
+		}
 	}
 }
